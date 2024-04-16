d9a7acb0b6ffe5edb9768844faac54d26869dab7
Ashwin Kumar <skywordservices@gmail.com>
2024-04-16 13:22:00 +0530

    now enabling font family again

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,10 @@ module.exports = {
     },
 
     extend: {
-      // fontFamily: {
-      //   Helvetica: "Helvetica Neue",
-      //   cursive: ['Dancing Script', 'cursive']
-      // },
+      fontFamily: {
+        Helvetica: "Helvetica Neue",
+        cursive: ['Dancing Script', 'cursive']
+      },
       //
       // boxShadow: {
       //   '3xl': 'rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;',
